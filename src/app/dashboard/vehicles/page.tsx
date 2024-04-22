01f7839700726f2c90facfe6dd2f2adf6da51e91
Peter karanja <peterkaranja60@gmail.com>
2024-04-22 05:42:55 -0500

    Update page.tsx

--- src/app/dashboard/vehicles/page.tsx
+++ src/app/dashboard/vehicles/page.tsx
@@ -6,26 +6,35 @@ import { columns } from "./components/columns";
 import VehicleForm from "./components/vehicle-form";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import Modal from "@/components/modal";
+import { useState } from "react";
+import { Plus } from "lucide-react";
 
 export default function VehicleListPage() {
   const pathname = usePathname();
+  const [showModal, setShowModal] = useState(false);
   return (
     <>
-      <main className="w-full h-full px-14">
+      <main className="w-full px-14">
         <div className="h-28 flex items-center justify-between">
           <div className="text-2xl font-bold capitalize">
             <h1>{pathname.slice(1)}</h1>
           </div>
 
-          <Link href="/dashboard/vehicles/create_vehicle">
-            <Button className="bg-green-700 hover:bg-green-600 capitalize">
-              Create new vehicle
-            </Button>
-          </Link>
+          <Button
+            className="bg-green-700 hover:bg-green-600 capitalize gap-2"
+            onClick={() => setShowModal(true)}
+          >
+            <Plus />
+            Create new vehicle
+          </Button>
         </div>
 
         {/* <DataTable columns={columns} data={} /> */}
       </main>
+      <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
+        <VehicleForm />
+      </Modal>
     </>
   );
 }
