76973ea0a34804198d2cd0a8ae500878bfb804f2
Peter karanja <peterkaranja60@gmail.com>
2024-04-22 05:42:28 -0500

    Update index.ts

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -4,32 +4,32 @@ const getCurrentYear = () => new Date().getFullYear();
 
 const phoneNumberValidation = z
   .string()
-  .regex(/^254\d{9}$/, "Phone number must start with 254 and be max 12 digits");
+  .regex(/^254\d{9}$/, "Must start with 254 and be max 12 digits");
 
 export const DriverSchema = z.object({
   firstName: z.string().min(1, {
-    message: "First name is required",
+    message: "Required",
   }),
   lastName: z.string().min(1, {
-    message: "Last name is required",
+    message: "Required",
   }),
-  nationalId: z.string().min(2, { message: "National ID is required" }),
+  nationalId: z.string().min(2, { message: "Required" }),
   phoneNumber: phoneNumberValidation,
   licenseNumber: z.string().min(2, {
-    message: "License number is required",
+    message: "Required",
   }),
   licenseExpiration: z.date().min(new Date(getCurrentYear(), 0, 1), {
-    message: "Insurance expiration date is required",
+    message: "Required",
   }),
   driverStatus: z.enum(["active", "suspended", "inactive"]).default("active"),
 });
 
 export const VehicleSchema = z.object({
   vehicleReg: z.string().min(1, {
-    message: "Vehicle registration is required",
+    message: "Required",
   }),
   insuranceExpiration: z.date().min(new Date(getCurrentYear(), 0, 1), {
-    message: "Insurance expiration date is required",
+    message: "Requiredf",
   }),
   vehicleStatus: z.enum(["active", "inactive"]),
   vehicleClass: z.enum(["I", "II", "III", "IV", "V"]),
@@ -53,4 +53,4 @@ export const VehicleDataSchema = z.object({
 });
 
 export type DriverData = z.infer<typeof DriverDataSchema>;
-export type VehicleData = z.infer<typeof VehicleDataSchema>
+export type VehicleData = z.infer<typeof VehicleDataSchema>;
