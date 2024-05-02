3172522b910e857b2743fdd6de9b909c2eb42aa2
Peter karanja <peterkaranja60@gmail.com>
2024-05-02 05:46:43 -0500

    Update columns.tsx

--- src/app/dashboard/vehicles/components/columns.tsx
+++ src/app/dashboard/vehicles/components/columns.tsx
@@ -1,12 +1,22 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { VehicleData } from "@/schema";
 import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal, SquarePen, Trash2 } from "lucide-react";
 
 export const columns: ColumnDef<VehicleData>[] = [
   {
     accessorKey: "vehicleReg",
     header: "vehicle Registration",
+    enableHiding: false,
   },
   {
     accessorKey: "insuranceExpiration",
@@ -20,5 +30,32 @@ export const columns: ColumnDef<VehicleData>[] = [
   {
     accessorKey: "vehicleClass",
     header: "Vehicle Class",
+  },
+  {
+    id: "actions",
+    header: "Management",
+    enableHiding: false,
+    cell: () => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="w-8 h-8 p-0">
+              <MoreHorizontal className="h-5 w-5" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem className="flex justify-between">
+              Update
+              <SquarePen className="w-4 h-4 text-blue-500" />
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex justify-between">
+              Delete
+              <Trash2 className="w-4 h-4 text-red-500" />
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
