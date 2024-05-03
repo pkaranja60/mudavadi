699418f18abc884a75bd6627b5b80cc78d6ebe07
Peter karanja <peterkaranja60@gmail.com>
2024-05-03 06:13:09 -0500

    Update index.ts

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -29,7 +29,45 @@ export const VehicleSchema = z.object({
     message: "Required",
   }),
   insuranceExpiration: z.date().min(new Date(getCurrentYear(), 0, 1), {
-    message: "Requiredf",
+    message: "Required",
+  }),
+  vehicleStatus: z.enum(["active", "inactive"]),
+  vehicleClass: z.enum(["I", "II", "III", "IV", "V"]),
+});
+
+export const ScheduleSchema = z.object({
+  driver: z.string().min(1, {
+    message: "Required",
+  }),
+  scheduleDateTime: z.date().min(new Date(getCurrentYear(), 0, 1), {
+    message: "Required",
+  }),
+  vehicle: z.string().min(1, {
+    message: "Required",
+  }),
+});
+
+export const DriverVehicleSchema = z.object({
+  firstName: z.string().min(1, {
+    message: "Required",
+  }),
+  lastName: z.string().min(1, {
+    message: "Required",
+  }),
+  nationalId: z.string().min(2, { message: "Required" }),
+  phoneNumber: phoneNumberValidation,
+  licenseNumber: z.string().min(2, {
+    message: "Required",
+  }),
+  licenseExpiration: z.date().min(new Date(getCurrentYear(), 0, 1), {
+    message: "Required",
+  }),
+  driverStatus: z.enum(["active", "suspended", "inactive"]).default("active"),
+  vehicleReg: z.string().min(1, {
+    message: "Required",
+  }),
+  insuranceExpiration: z.date().min(new Date(getCurrentYear(), 0, 1), {
+    message: "Required",
   }),
   vehicleStatus: z.enum(["active", "inactive"]),
   vehicleClass: z.enum(["I", "II", "III", "IV", "V"]),
@@ -52,5 +90,20 @@ export const VehicleDataSchema = z.object({
   vehicleClass: z.string(),
 });
 
+export const DriverDataCopySchema = z.object({
+  lastName: z.string(),
+  firstName: z.string(),
+  phoneNumber: z.string(),
+  nationalId: z.string(),
+  licenseNumber: z.string(),
+  licenseExpiration: z.date(),
+  driverStatus: z.string(),
+  vehicleReg: z.string(),
+  insuranceExpiration: z.date(),
+  vehicleStatus: z.string(),
+  vehicleClass: z.string(),
+});
+
 export type DriverData = z.infer<typeof DriverDataSchema>;
 export type VehicleData = z.infer<typeof VehicleDataSchema>;
+export type DriverDataCopy = z.infer<typeof DriverDataCopySchema>;
