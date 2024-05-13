7258cf16040852389323d7b569e355c490af6880
Peter karanja <peterkaranja60@gmail.com>
2024-05-13 06:33:46 -0500

    Update route.ts

--- src/app/api/send-sms/route.ts
+++ src/app/api/send-sms/route.ts
@@ -1,26 +1,43 @@
 import { NextResponse, NextRequest } from "next/server";
 const AfricasTalking = require("africastalking");
 
+type SendSMSRequest = {
+  phone: string;
+  message: string;
+};
+
 export async function POST(request: NextRequest) {
   try {
+    const data: SendSMSRequest = await request.json(); // Destructure with typed data
+    console.log("Sending SMS to:", data.phone);
+
     const africastalking = AfricasTalking({
       apiKey: String(process.env.NEXT_PUBLIC_AFRICAS_TALKING_API),
       username: String(process.env.NEXT_PUBLIC_AFRICAS_TALKING_USERNAME),
     });
 
-    const { phone, message }: any = await request.json(); // Destructure formData from request body
-    console.log("Sending SMS to:", phone);
-
     const result = await africastalking.SMS.send({
-      message: message,
+      message: data.message,
       from: "78485",
-      to: phone,
+      to: data.phone,
     });
 
     console.log("SMS sent successfully:", result.sid);
     return NextResponse.json({ message: "success" }, { status: 200 });
-  } catch (error) {
+  } catch (error: any) {
     console.error("Error sending SMS:", error);
-    return NextResponse.error();
+    if (error.response && error.response.status) {
+      // Handle specific errors from Africa's Talking API
+      return NextResponse.json(
+        { message: error.message },
+        { status: error.response.status }
+      );
+    } else {
+      // Handle other errors (e.g., server error)
+      return NextResponse.json(
+        { message: "Internal Server Error" },
+        { status: 500 }
+      );
+    }
   }
 }
