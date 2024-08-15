49f4a9434a65646ce22569dbed5cab16fada34ca
Peter karanja <peterkaranja60@gmail.com>
2024-08-15 17:04:41 -0500

    Update layout.tsx

--- src/app/(client)/dashboard/layout.tsx
+++ src/app/(client)/dashboard/layout.tsx
@@ -1,11 +1,9 @@
-import BottomNavbar from "@/components/ui/bottomnav";
-import Navbar from "@/components/ui/nav";
-import Sidebar from "@/components/ui/sidebar";
+import BottomNavbar from "@/components/BottomNavbar";
+import Navbar from "@/components/Navbar";
 
 export default function Layout({ children }: any) {
   return (
     <main className="min-w-max min-h-max">
-      {/* <Sidebar /> */}
       <Navbar />
       <div className="max-w-screen-2xl mx-5 md:mx-14 xl:mx-auto mt-20">{children}</div>
 
