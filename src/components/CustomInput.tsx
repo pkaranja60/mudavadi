b4495b745f70fbe0c2b7bb35f272707db0d88e77
Peter karanja <peterkaranja60@gmail.com>
2024-08-15 17:05:05 -0500

    Create CustomInput.tsx

---
+++ src/components/CustomInput.tsx
@@ -0,0 +1,49 @@
+import {
+  FormControl,
+  FormField,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { applicationFormSchema } from "@/schema";
+import { Control, FieldPath } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = applicationFormSchema("driver");
+
+interface CustomInputProps {
+  control: Control<z.infer<typeof formSchema>>;
+  name: FieldPath<z.infer<typeof formSchema>>;
+  label: string;
+  placeholder?: string;
+}
+
+export default function CustomInput({
+  control,
+  name,
+  label,
+  placeholder,
+}: CustomInputProps) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex w-full flex-col">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                className="input-class"
+                type={"text"}
+                {...field}
+              />
+            </FormControl>
+            <FormMessage className="form-message mt-2" />
+          </div>
+        </div>
+      )}
+    />
+  );
+}
