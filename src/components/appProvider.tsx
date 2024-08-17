99816034db4a1139c16ddb4317d04d9af216d331
Peter karanja <peterkaranja60@gmail.com>
2024-08-17 14:39:36 -0500

    Update appProvider.tsx

--- src/components/appProvider.tsx
+++ src/components/appProvider.tsx
@@ -2,12 +2,14 @@
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
 import { Toaster } from "sonner";
 
 // Create a client
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 export default function AppProvider({ children }: any) {
+  const [queryClient] = React.useState(() => new QueryClient());
   return (
     <ClerkProvider>
       <QueryClientProvider client={queryClient}>
